1e74919ffdec1413c059fc92b956b65deaab3e05
Jesus Efren Cabrera Ortega <efren507.507@gmail.com>
2019-05-22 17:30:35 -0700

    Agrega primer versión de la función para descargar datos de bitbucket

---
+++ descarga_datos/get_data.py
@@ -0,0 +1,37 @@
+import urllib.request
+
+
+def download_file_from_repo(url: str, destionation_filename: str, user: str, passwd: str):
+    """
+    Función que permite descargar archivos desde repositorios de bitbucket
+
+    Parámetros
+    ----------
+    `url : str`
+        Dirección de Bitbucket donde se encuentra el archivo
+
+    `destionation_filename: str`
+        Nombre y dirección del archivo donde se guardarán los datos descargados
+
+    `user : str`
+        Nombre de usuario de Bitbucket
+
+    `passwd : str`
+        Contraseña del usuario
+    
+    Notas
+    -----
+    Ninguna
+    
+    Ejemplos
+    --------
+    Descargar un archivo
+    >>> url = 'https://bitbucket.org/usuario_prueba/repo_datos/raw/9fd54/datos.xlsx'
+    >>> download_file(url, 'inst/extdata/datos.xlsx', os.environ['BITBUCKET_USERNAME'],
+    ... os.environ['BITBUCKET_PASSWORD'])
+    """
+    manejador_autenticacion = urllib.request.HTTPBasicAuthHandler()
+    manejador_autenticacion.add_password(None, uri=url, user=user, passwd=passwd)
+    conexion = urllib.request.build_opener(manejador_autenticacion)
+    urllib.request.install_opener(conexion)
+    urllib.request.urlretrieve(url, filename)
