b2114ac0ea1158423f32b91f411b6ceb47f998b5
Nepo <braulio.rojas@islas.org.mx>
2019-07-09 16:29:00 -0700

    Agrega primera versión de la cli

---
+++ descarga_datos/cli.py
@@ -0,0 +1,17 @@
+from .internals import Analysis
+from .network import download_file_from_repo
+from .utils import get_password_from_enviormet_variable, get_user_from_enviorment_variable
+import sys
+import json
+
+def descarga_archivo(file_name, destination_folder):
+    with open('analyses.json') as json_analyses:
+        lista_analisis = json.load(json_analyses)
+    for diccionario_analisis in lista_analisis:
+        analisis = Analysis(**diccionario_analisis)
+        if analisis.is_dependent_on_datafile(file_name):
+            url = analisis.get_url_to_datafile(file_name)
+            download_file_from_repo(url, destination_folder, get_user_from_enviorment_variable(), get_password_from_enviormet_variable())
+
+def cli():
+    descarga_archivo(*sys.argv[1:])
