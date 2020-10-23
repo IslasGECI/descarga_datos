73a068e7eefd41cc695e11934a9dec452cbfd98c
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-10-23 12:06:11 -0700

    :nail_care: Encapsula variables

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -8,8 +8,12 @@ os.environ["BITBUCKET_PASSWORD"] = "password"
 
 
 def test_get_user_from_enviorment_variable():
-    assert os.environ["BITBUCKET_USERNAME"] == get_user_from_enviorment_variable()
+    obtained_user = get_user_from_enviorment_variable()
+    expected_user = os.environ["BITBUCKET_USERNAME"]
+    assert expected_user == obtained_user
 
 
 def test_get_password_from_enviormet_variable():
-    assert os.environ["BITBUCKET_PASSWORD"] == get_password_from_enviormet_variable()
+    obtained_password = get_password_from_enviormet_variable()
+    expected_password = os.environ["BITBUCKET_PASSWORD"]
+    assert expected_password == obtained_password
