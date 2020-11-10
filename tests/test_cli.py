cb48eadcea80bc4b245b50abd037ad6270915df6
Xavier Paredes <jxavierpp1@gmail.com>
2020-11-10 14:55:51 -0700

    🧪 agrega prueba para el cli con argumentos

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,10 @@
 from descarga_datos.cli import descarga_archivo, cli
-
+import sys
 
 def test_descarga_archivo():
     descarga_archivo(".","./results")
+
+
+def test_cli():
+    sys.argv = ["cli.py", ".", "./results"]
+    cli()
