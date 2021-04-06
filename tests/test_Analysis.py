7a4fee9a921a898b32d5cc602213f03e9638c73a
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-04-06 09:13:34 -0700

    🛑 Add failed test

--- tests/test_Analysis.py
+++ tests/test_Analysis.py
@@ -18,6 +18,43 @@ TEXTO_ANALYSIS = """[{
             "filename": "roedores_capturarecaptura_cedros.csv",
             "version": "d2ca5a04850b",
             "type": "datapackage"
+        },
+        {
+            "source": "tabular_data_packages",
+            "version": "1162b173",
+            "path": "camaras_trampa_gatos_isla_guadalupe",
+            "type": "json",
+            "filename": "datapackage.json"
+        }
+
+    ],
+    "requirements": []
+},
+{
+    "docker_parent_image": "islasgeci/jupyter:5b83",
+    "name": "Densidad de madrigueras de mérgulo a partir de busquedas exhaustivas",
+    "description": "Mapas de densidad de madrigueras de mérgulo en Isla Guadalupe durante las temporadas 2014-2018.",
+    "report": "mapa_densidad_censo.pdf",
+    "results": [
+        "densidad_mergulo_todas_temporadas_zapato.png"
+    ],
+    "scripts": [
+        "src/plot_burrows_density_map"
+    ],
+    "data": [
+        {
+            "source": "tabular_data_packages",
+            "version": "b205951ba7d1720891edc9645cf5eceb669fdda6",
+            "path": "nidos_busqueda_aves_marinas",
+            "type": "json",
+            "filename": "datapackage.json"
+        },
+        {
+            "source": "archivos_binarios",
+            "version": "d60fea2117a3c2f23be256ff34c112e1a2bd957c",
+            "path": "shp/guadalupe",
+            "type": "datapackage",
+            "filename": "linea_costa_isla_guadalupe.shp"
         }
     ],
     "requirements": []
@@ -54,6 +91,21 @@ def test_get_url_to_datafile():
     obtained_url = analisis.get_url_to_datafile("roedores_capturarecaptura_cedros.csv")
     assert obtained_url == (expected_url)
     assert analisis.get_url_to_datafile("rodores_capturarecaptura_cedros.csv") is None
+
+
+def test_get_url_to_datafile_two_paths():
+    expected_url = (
+        "https://bitbucket.org/IslasGECI/tabular_data_packages/raw/1162b173/"
+        + "camaras_trampa_gatos_isla_guadalupe/datapackage.json"
+    )
+    obtained_url = analisis.get_url_to_datafile("datapackage.json")
+    assert obtained_url == (expected_url)
+    expected_url = (
+        "https://bitbucket.org/IslasGECI/tabular_data_packages/raw/d60fea2117/"
+        + "nidos_busqueda_aves_marinas/datapackage.json"
+    )
+    obtained_url = analisis.get_url_to_datafile("datapackage.json")
+    assert obtained_url == (expected_url)
 
 
 def test_init():
