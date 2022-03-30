26939483d051fba58a341e7461f66d7605142856
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-03-30 10:00:32 -0700

    🛑 Improve error messages

--- tests/test_version.sh
+++ tests/test_version.sh
@@ -5,7 +5,8 @@ if [ "${obtained_version}" = "${expected_version}" ]; then
   echo "."
   exit 0
 else
-  echo "F"
+  echo "Versión en la rama: v{obtained_version}"
+  echo "Versión en 'setup.py': v{expected_version}"
   echo "FAIL: Las versiones no coinciden"
   exit 1
 fi
