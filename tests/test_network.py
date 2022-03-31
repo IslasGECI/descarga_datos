dc7135f7049e7e5f514aa78259ba3f457ca6805f
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-03-31 15:21:49 -0700

    ✅ Tests that can download a zip file

--- tests/test_network.py
+++ tests/test_network.py
@@ -17,3 +17,15 @@ def test_download_file_from_repo():
     url = archivo.get_url_to_file()
     destination_folder = "./results"
     download_file_from_repo(url, destination_folder)
+    file_name = "clarion_vegetal_types.zip"
+    archivo = descarga_datos.internals.DataFile(
+        "archivos_binarios",
+        "shp/clarion_vegetal_zones",
+        file_name,
+        "aeafdde",
+        "zip",
+    )
+    url = archivo.get_url_to_file()
+    download_file_from_repo(url, destination_folder)
+    file_size = os.path.getsize(f"{destination_folder}/{file_name}")
+    assert file_size > 200
