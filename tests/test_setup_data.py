42fbbba78f536ca8fb2d7c40aec9391b74f901f3
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-08-07 20:40:49 +0000

    🛑🧪 Fail tests

--- tests/test_setup_data.py
+++ tests/test_setup_data.py
@@ -3,8 +3,13 @@ from descarga_datos import filter_date_by_condition
 import pandas as pd
 
 
+conditional_year = "< 2021"
+
+
 def test_filter_date_by_condition():
     data_path = "tests/data/nidos_busqueda_aves_marinas.csv"
     data_to_filter = pd.read_csv(data_path)
-    conditional_year = "< 2021"
-    filter_date_by_condition(data_to_filter, conditional_year)
+    obtained_filtered_data = filter_date_by_condition(data_to_filter, conditional_year)
+    expected_filtered_rows = 13
+    obtained_filtered_data_length = len(obtained_filtered_data)
+    assert obtained_filtered_data_length == expected_filtered_rows
