703ad1edfb82d045971fcdc34a1beb4661643061
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-08-07 20:48:19 +0000

    ✅ Pass tests

--- descarga_datos/internals/setup_data.py
+++ descarga_datos/internals/setup_data.py
@@ -1,2 +1,3 @@
 def filter_date_by_condition(data_to_filter, conditional_year):
-    pass
+    data_to_filter["year"] = data_to_filter["Fecha"].str.slice(7, 11).astype(int)
+    return data_to_filter.query("year " + conditional_year)
