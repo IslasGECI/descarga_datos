2db3e11ddf135d9d5586d736f0a083fd39074c86
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-08-08 16:08:28 +0000

    🛑🧪 Fail tests

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -12,5 +12,15 @@ def test_setup_cli():
     tdp_path = "nidos_busqueda_aves_marinas"
     os.system(f"descarga_datos {file_to_download} {destination_folder} {tdp_path}")
 
-    result = runner.invoke(app, ["--file-data-name", destination_folder + "/" + file_to_download])
+    result = runner.invoke(
+        app,
+        [
+            "--file-data-name",
+            destination_folder + "/" + file_to_download,
+            "--report",
+            "tamanio_poblacional",
+            "--analysis",
+            "tests/data/analysis_tamanio.json",
+        ],
+    )
     assert result.exit_code == 0
