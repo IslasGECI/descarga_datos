996512139504db068c3a8a507a1ec70b5c8a0372
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-08-08 18:53:31 +0000

    ✅ Pass tests

--- descarga_datos/app.py
+++ descarga_datos/app.py
@@ -11,4 +11,4 @@ def setup_data(file_data_name: str = "", report: str = "", analysis: str = "anal
     data_to_filter = pd.read_csv(file_data_name)
     analyses_list = read_json(analysis)
     filtered_data = setup_data_by_report(data_to_filter, report, analyses_list)
-    filtered_data.to_csv(file_data_name)
+    filtered_data.to_csv(file_data_name, index=False)
