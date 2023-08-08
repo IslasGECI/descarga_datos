684b4363dc0cb8755202aa7d08766c092f469688
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-08-08 17:49:16 +0000

    🛑🧪 Fail tests

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,6 +1,7 @@
 from descarga_datos.app import app
 
 import os
+import pandas as pd
 from typer.testing import CliRunner
 
 runner = CliRunner()
@@ -12,11 +13,14 @@ def test_setup_cli():
     tdp_path = "nidos_busqueda_aves_marinas"
     os.system(f"descarga_datos {file_to_download} {destination_folder} {tdp_path}")
 
+    data_path = destination_folder + "/" + file_to_download
+    original_data = pd.read_csv(data_path)
+
     result = runner.invoke(
         app,
         [
             "--file-data-name",
-            destination_folder + "/" + file_to_download,
+            data_path,
             "--report",
             "tamanio_poblacional",
             "--analysis",
@@ -24,3 +28,5 @@ def test_setup_cli():
         ],
     )
     assert result.exit_code == 0
+    filtered_data = pd.read_csv(data_path)
+    assert len(filtered_data) < len(original_data)
