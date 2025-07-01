f747a05978304855adea537ea5e9d573dce939a6
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-01 18:21:00 +0000

    ✅ Pass tests

--- descarga_datos/cli.py
+++ descarga_datos/cli.py
@@ -1,5 +1,6 @@
 from .internals import Analysis, read_json
 from .network import download_file_from_repo
+import pandas as pd
 import sys
 
 
@@ -11,6 +12,10 @@ def descarga_archivo(file_name, destination_folder, path):
             download_file_from_repo(
                 analisis.get_url_to_datafile(path, file_name), destination_folder
             )
+            file_path = f"{destination_folder}/{file_name}"
+            file_df = pd.read_csv(file_path)
+            file_df.rename(columns={"Date": "Fecha"}, inplace=True)
+            file_df.to_csv(file_path, index=False)
 
 
 def cli():
