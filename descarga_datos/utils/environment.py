3b26bf214776277ee1e5de6e1c291d230c4fe401
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2026-06-22 21:24:16 -0700

    ♻️ Translate environment.py docstrings to English and remove empty sections

--- descarga_datos/utils/environment.py
+++ descarga_datos/utils/environment.py
@@ -3,83 +3,50 @@ import os
 
 def get_user_from_environment_variable() -> str:
     """
-    Función que regresa el nombre de usuario de Bitbucket desde la variable de
-    entorno BITBUCKET_USERNAME
+    Return the Bitbucket username from the BITBUCKET_USERNAME environment
+    variable.
 
-    Parámetros
-    ----------
-    Ninguno
-
-    Notas
-    -----
-    Ninguna
-
-    Ejemplos
+    Examples
     --------
-    Obtener nombre de usuario
-    >>> usuario = descarga_datos.util.get_user_from_environment_variable()
+    Get username
+    >>> user = descarga_datos.get_user_from_environment_variable()
     """
     return os.environ["BITBUCKET_USERNAME"]
 
 
 def get_password_from_environment_variable() -> str:
     """
-    Función que regresa la contraseña de Bitbucket desde la variable de entorno
-    BITBUCKET_PASSWORD
+    Return the Bitbucket password from the BITBUCKET_PASSWORD environment
+    variable.
 
-    Parámetros
-    ----------
-    Ninguno
-
-    Notas
-    -----
-    Ninguna
-
-    Ejemplos
+    Examples
     --------
-    Obtener contraseña del usuario
-    >>> contrasenia = descarga_datos.util.get_password_from_environment_variable()
+    Get password
+    >>> password = descarga_datos.get_password_from_environment_variable()
     """
     return os.environ["BITBUCKET_PASSWORD"]
 
 
 def get_token_from_environment_variable() -> str:
     """
-    Función que regresa el token de Bitbucket desde la variable de entorno
-    BITBUCKET_API_TOKEN
+    Return the Bitbucket API token from the BITBUCKET_API_TOKEN environment
+    variable.
 
-    Parámetros
-    ----------
-    Ninguno
-
-    Notas
-    -----
-    Ninguna
-
-    Ejemplos
+    Examples
     --------
-    Obtener token
-    >>> token = descarga_datos.util.get_token_from_environment_variable()
+    Get token
+    >>> token = descarga_datos.get_token_from_environment_variable()
     """
     return os.environ["BITBUCKET_API_TOKEN"]
 
 
 def get_email_from_environment_variable() -> str:
     """
-    Función que regresa el correo electrónico de Bitbucket desde la variable de
-    entorno BITBUCKET_EMAIL
+    Return the Bitbucket email from the BITBUCKET_EMAIL environment variable.
 
-    Parámetros
-    ----------
-    Ninguno
-
-    Notas
-    -----
-    Ninguna
-
-    Ejemplos
+    Examples
     --------
-    Obtener correo electrónico
-    >>> email = descarga_datos.util.get_email_from_environment_variable()
+    Get email
+    >>> email = descarga_datos.get_email_from_environment_variable()
     """
     return os.environ["BITBUCKET_EMAIL"]
